22033c8573694ad4f10e23e71ed9573070c15429
KirillDanProg <kirill2898v@gmail.com>
2023-09-27 18:07:15 +0400

    add: additional fields added to expense

--- models/user-model.ts
+++ models/user-model.ts
@@ -1,27 +1,33 @@
-import mongoose, { Schema } from 'mongoose'
+import mongoose, { Schema } from "mongoose";
 
-interface User {
-    name: string
-    password: string
+export interface User {
+  email: string;
+  name: string;
+  password: string;
 }
 
 const Expense = {
-    id: { type: String, required: true },
-    title: { type: String, required: true },
-    amount: { type: Number, required: true }
-}
+  id: { type: String, required: true },
+  title: { type: String, required: true },
+  amount: { type: Number, required: true },
+  category: String,
+  description: String,
+  date: Date,
+};
 
 const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    expenses: [ Expense ],
-})
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  image: String,
+  expenses: [Expense],
+});
 
-export const User = mongoose.models.User || mongoose.model('User', userSchema)
+export const User = mongoose.models.User || mongoose.model("User", userSchema);
