047e895d1a643fcd00afef0a308daeb805bcd60d
KirillDanProg <kirill2898v@gmail.com>
2023-09-27 18:06:31 +0400

    add: post function to add expense to user

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,14 +1,34 @@
-import { User } from "@/models/user-model"
-import { connectMongoDB } from "@/utils/database"
-import { NextResponse } from "next/server"
+import { User } from "@/models/user-model";
+import { connectMongoDB } from "@/utils/database";
+import { NextResponse } from "next/server";
 
 //todo: find types for second argument
-export async function DELETE(request: Request, {params}: {params: any}) {
-    try {
-        await connectMongoDB()
-        await User.findByIdAndDelete(params.id)
-        return NextResponse.json({message: "User deleted"})
-    } catch (error) {
-        console.log("error while deleting user")
+export async function DELETE(request: Request, { params }: { params: any }) {
+  try {
+    await connectMongoDB();
+    await User.findByIdAndDelete(params.id);
+    return NextResponse.json({ message: "User deleted" });
+  } catch (error) {
+    console.log("error while deleting user");
+  }
+}
+
+export async function POST(request: Request, { params }: { params: any }) {
+  try {
+    const { id } = params
+    const newExpense = await request.json()
+    await connectMongoDB();
+    const user = await User.findById(id)
+    if (!user) {
+      throw new Error("user not found")
     }
+    const find = {
+      _id: id
+    }
+    const update = { $push: { expenses: newExpense } }
+    await User.findOneAndUpdate(find, update)
+    return NextResponse.json({ message: "expenses added" });
+  } catch (error) {
+    console.log("error while adding expense");
+  }
 }
