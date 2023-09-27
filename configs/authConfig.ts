577d7b97b3ab373b9b66b00bee96ef976414c94c
KirillDanProg <kirill2898v@gmail.com>
2023-09-27 18:03:17 +0400

    add: create user after google authorization

--- configs/authConfig.ts
+++ configs/authConfig.ts
@@ -1,12 +1,38 @@
-import { NextAuthOptions } from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { User } from "@/models/user-model";
+import { connectMongoDB } from "@/utils/database";
 
 const authOptions: NextAuthOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET,
-        })
-    ],
-}
-export default authOptions
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({ email: session.user?.email });
+      session.user.id = sessionUser?._id.toString();
+      return session;
+    },
+    async signIn({ user }) {
+      try {
+        await connectMongoDB();
+        const userExists = await User.findOne({ email: user.email });
+        if (!userExists) {
+          await User.create({
+            email: user.email,
+            name: user.name,
+            image: user.image,
+          });
+        }
+        return true;
+      } catch (e) {
+        console.log("Error while signing in");
+        return false;
+      }
+    },
+  },
+};
+export default authOptions;
