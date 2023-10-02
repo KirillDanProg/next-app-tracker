e86721168f7df202e5775d9f7cc8f4933acbb31c
KirillDanProg <kirill2898v@gmail.com>
2023-10-02 21:57:46 +0300

    add: redux devtools added

--- state/store.ts
+++ state/store.ts
@@ -1,15 +1,16 @@
-import {configureStore} from '@reduxjs/toolkit'
-import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
-import expensesReducer from '@/state/features/expense-slice'
+import { configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import expensesReducer from "@/state/features/expense-slice";
 
-const store = configureStore({
-    reducer: {
-        expenses: expensesReducer
-    }
-})
+export const store = configureStore({
+  reducer: {
+    expenses: expensesReducer,
+  },
+  devTools: true
+});
 
-type RootState = ReturnType<typeof store.getState>
-type AppDispatch = typeof store.dispatch
+type RootState = ReturnType<typeof store.getState>;
+type AppDispatch = typeof store.dispatch;
 
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
-export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch: () => AppDispatch = useDispatch;
