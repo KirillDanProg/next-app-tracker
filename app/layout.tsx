976d5f513be82ebc7c9afa028829752a553d8886
KirillDanProg <kirill2898v@gmail.com>
2023-10-02 21:57:16 +0300

    feat: store provider added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,32 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import React from "react";
-import Provider from "@/components/Provider";
+import Providers from "@/components/Providers";
 import Navbar from "@/components/Navbar";
 
-const inter = Inter({ subsets: [ 'latin' ] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: 'Трэкер трат',
-    description: 'Приложения для рассчета трат',
-}
+  title: "Трэкер трат",
+  description: "Приложения для рассчета трат",
+};
 
-export default function RootLayout({ children, }: { children: React.ReactNode }) {
-    return (
-        <html lang="en">
-        <body className={inter.className}>
-        <Provider>
-            <Navbar/>
-            <main className='min-h-screen flex justify-center blue_gradient'>
-                <div className='w-4/5 flex justify-center'>
-                    {children}
-                </div>
-            </main>
-        </Provider>
-        </body>
-        </html>
-    )
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>
+          <Navbar />
+          <main className="min-h-screen flex justify-center blue_gradient">
+            <div className="w-4/5 flex justify-center">{children}</div>
+          </main>
+        </Providers>
+      </body>
+    </html>
+  );
 }
