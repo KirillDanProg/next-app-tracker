8af1d1c5c0dca5db83c2e474ad986e9324e70914
KirillDanProg <kirill2898v@gmail.com>
2023-10-02 21:56:25 +0300

    feat: addExpenseThunk added

--- state/features/expense-slice.ts
+++ state/features/expense-slice.ts
@@ -1,16 +1,12 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { FormDataType } from "@/components/AddExpanseForm";
+import { Expense } from "@/components/AddExpanseForm";
 
 type InitialState = {
     expenses: Expense[];
     status: Status;
     error: string | string[];
 };
-type Expense = {
-    id: "";
-    title: "";
-    amount: 0;
-};
+
 type Status = "idle" | "loading" | "success" | "failed";
 
 const initialState = {
@@ -23,13 +19,40 @@ const expenses = createSlice({
     name: "expenses",
     initialState,
     reducers: {
+        addExpense: (state, {payload}: {payload: Expense}) => {
+            state.expenses.push(payload)
+        },
         deleteExpense: (state, action) => {
+
         },
         editExpense: () => {
         },
     },
+    // extraReducers: (builder) => {
+    //     builder
+    //         .addCase(addExpenseThunk.fulfilled, (state, action) => {
+    //             debugger
+    //         })
+    // }
 });
 
-export const
- { editExpense, deleteExpense } = expenses.actions;
+export const { editExpense, deleteExpense, addExpense } = expenses.actions;
+export const addExpenseThunk = createAsyncThunk<any, { id: string | undefined, formData: Expense }>(
+    'expenses/addExpense', async ({ id, formData }, thunkAPI) => {
+        if (!id) {
+            return thunkAPI.rejectWithValue("error")
+        }
+        const res = await fetch(`http://localhost:3000/api/users/${id}`, {
+            method: 'POST',
+            cache: "no-cache",
+            body: JSON.stringify(formData)
+        })
+        if (!res.ok) {
+            return thunkAPI.rejectWithValue("error")
+        }
+        thunkAPI.dispatch(addExpense(formData))
+        return res.json()
+    }
+)
+
 export default expenses.reducer;
