d6ea4731b0a3fae27600d55accfb351c5bf70493
KirillDanProg <kirill2898v@gmail.com>
2023-10-07 08:45:00 +0300

    add: getExpenses function created at expenses page

--- app/expenses/[id]/page.tsx
+++ app/expenses/[id]/page.tsx
@@ -1,25 +1,37 @@
-import type { InferGetStaticPropsType, GetStaticProps } from 'next'
-import type { Expense } from '@/types/expense'
-import expenses from '../../../state/features/expense-slice';
-
-export const getExpenses = async (id: string) => {
-  console.log(id)
-  const res = await fetch(`http://localhost:3000/api/users/${id}/expenses`)
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-  return res.json()
-}
+import { Expense } from "@/types/expense";
+import { ExpensesPieHart } from "@/components/expenses-pie-сhart";
 
 const Expenses = async ({ params }: { params: { id: string } }) => {
-  const { id } = params
-  const expenses = await getExpenses(id)
-  console.log(expenses)
-  return (
-    <div>
-      rweqrqwe
-    </div>
-  )
+    const { id } = params
+    const data = await getExpenses(id)
+
+    if ("error" in data) {
+        return <div>{data.error}</div>
+    }
+    return (
+        <section className="flex flex-col gap=4 text-white">
+            <ExpensesPieHart expenses={data.expenses}/>
+            {
+                data.expenses?.map(el => <div className={'flex flex-col'} key={el.title}>
+                        <div className={"flex"}>
+                            <h6>{el.title}</h6>
+                            <p>{el.amount}</p>
+                        </div>
+                    </div>
+                )
+            }
+        </section>
+    )
+}
+
+export async function getExpenses(id: string): Promise<{ expenses: Expense[] } | { error: string }> {
+    const res = await fetch(`http://localhost:3000/api/users/${id}/expenses`)
+    if (!res.ok) {
+        return {
+            error: res.statusText
+        }
+    }
+    return await res.json()
 }
 
 export default Expenses
